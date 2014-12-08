0dfdbd1bee7fe025a201c4d0fc7acbacf36c8327
nyand <rduo3@student.monash.edu.au>
2014-12-08 12:31:13 +1100

    added loading of data files

--- text_adventure.rb
+++ text_adventure.rb
@@ -2,26 +2,34 @@ require 'json'
 class TextAdventure
 
   def initialize
-    load_config("file_config.json")
-    load_rooms
-    load_characters
+    data = load_config "file_config.json"
+    load_rooms data['room_location'] 
+    load_characters data['character_location']
   end
 
   private 
-    def load_rooms(file_path)
+    def load_rooms(file_path = 'rooms' )
+      puts file_path
+      Dir.foreach(file_path) do |file|
+        next if file == '.' or file == '..'
+        
+        puts file
+        puts JSON.parse File.read file
+      end
     end
 
-    def load_characters(file_path)
+    def load_characters(file_path = 'charaacters')
     end
+
 
     def load_config(file)
-      config_file = JSON.parse(file)
+      JSON.parse(File.read(file))
+    end
 
-      puts config_file 
-
-      config_file.each do |config_data|
-        puts config_data
+    def read_files(dir = '.')
+      Dir.foreach(dir) do |file|
+        next if file == '.' || file == '..'
+        File.open(file,'r') { |file_data| yield }
       end
-
-  end
+    end
 end
